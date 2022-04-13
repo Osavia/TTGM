2117899b970614b2bbcc3149b36d245ba51e8a81
Osavia <piquer.alexis@gmail.com>
2022-04-13 12:52:19 +0200

    mise à jour css

--- MVC/views/_header.php
+++ MVC/views/_header.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./images/TTGM-Logo-vert.png" type="image/x-icon">
-    <link rel="stylesheet" href="./styles/css/style.css">
+    <link rel="stylesheet" href="/styles/css/style.css">
     <title>Train de Guîtres</title>
 
 </head>
