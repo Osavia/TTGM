f39c9979f5cc040562f6e2ff6ee77cc55a8de80b
Osavia <piquer.alexis@gmail.com>
2022-04-15 09:36:34 +0200

    commit

--- MVC/views/actualite/actualite.php
+++ MVC/views/actualite/actualite.php
@@ -27,11 +27,7 @@
 
 </div>
 
-<<<<<<< HEAD
-<!-- Liste des articles : -->
-=======
 <!-- Affichage de la liste des articles : -->
->>>>>>> dev
 
 <div>
     <table>
@@ -54,11 +50,7 @@
                                 <a href="?page=selected_article&id=<?= $article->getId() ?>" title="Modifier cet article">Modifier</a>
                             </td>
                             <td>
-<<<<<<< HEAD
-                                <button class="displayBtns">Supprimer</button>
-=======
                                 <button class="displayBtns" data-title_article="<?= $article->getTitle() ?>" data-id_article="<?= $article->getId() ?>">Supprimer</button>
->>>>>>> dev
                             </td>
                             <?php } ?>
                     </tr>
@@ -68,24 +60,6 @@
     </table>
 </div>
 
-<<<<<<< HEAD
-
-<?php
-var_dump($article->getId()) ;
-var_dump($article->getTitle()) ;
-?>
-
-<!-- Modale de confirmation de suppresion : -->
-
-<div class="myPopup">
-    <div class="row">
-        <h2>Voulez-vous vraiment supprimer cet article : <?= $article->getTitle() ?> ?</h2>
-
-        <a href="?page=delete_article&id=<?= $article->getId() ?>">Oui</a>
-        <span id="closePopup">Non</span>
-    </div>
-
-=======
 <!-- Modale de confirmation de suppresion : -->
 
 <div class="myPopup">
@@ -98,5 +72,4 @@ var_dump($article->getTitle()) ;
         </a>
         <span id="closePopup">Non</span>
     </div>
->>>>>>> dev
 </div>
